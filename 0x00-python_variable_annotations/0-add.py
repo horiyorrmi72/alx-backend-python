82152d8b63b2f4a820dcb26b227fcdc01f6c3e48
yusuf ridwan <horiyorrmi72@gmail.com>
2022-12-26 07:07:15 +0000

    pycodestyle validation

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,2 +1,5 @@
+"""function add that takes a float as arguments and returnssum as a float."""
+
+
 def add(a: float, b: float) -> float:
     return a + b
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,2 +1,5 @@
-def concat(str1:str, str2:str) ->str:
+"""func concat that takes strings as arguments returns concatenated string"""
+
+
+def concat(str1: str, str2: str) -> str:
     return str1 + str2
