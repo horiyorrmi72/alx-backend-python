2b17362cc398947ce4d99fc9dc3d1822e58cb21c
yusuf ridwan <horiyorrmi72@gmail.com>
2022-12-26 07:23:38 +0000

    fix

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,7 @@
-"""func concat that takes strings as arguments returns concatenated string"""
+#!/usr/bin/env python3
+""" Module to concat strings"""
 
 
 def concat(str1: str, str2: str) -> str:
+    """ a method that concatinates two strings"""
     return str1 + str2
