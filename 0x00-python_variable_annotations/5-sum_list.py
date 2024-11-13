0695c80295bf50f19445aa8bbc31774ce0866990
yusuf ridwan <horiyorrmi72@gmail.com>
2024-11-13 13:42:44 +0000

    fix(docs):this commit will fix the documentation error

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 from typing import List
-"""Type-annotated function"""
+"""Module with type-annotated function to sum a list of provided floats."""
 
 
 def sum_list(input_list: List[float]) -> float:
+    """Return the sum of a list of floats."""
     return sum(input_list)
