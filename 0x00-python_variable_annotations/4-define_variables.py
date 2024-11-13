588ea607f2f82c0d7634548f8b5607ac745e9ea4
yusuf ridwan <horiyorrmi72@gmail.com>
2024-11-13 11:40:45 +0000

    fix(pycodestyle): this commit will fix whitespacing after the colon before the data type and to match pycodestyle

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-"""Varibles annotations"""
+"""Variable annotations"""
 
-
-a:int = 1
-pi:float = 3.14
-i_understand_annotations:bool = True
-school:str = "Holberton"
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
