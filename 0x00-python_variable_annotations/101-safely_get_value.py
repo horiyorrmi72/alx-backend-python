0f5107a01661818dbb15346450a647d0abe98bbd
yusuf ridwan <horiyorrmi72@gmail.com>
2024-11-15 10:58:42 +0000

    feat(type var): this commit will make use of typevar

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from typing import Mapping, Any, TypeVar, Union
+
+T = TypeVar('T')
+
+
+def safely_get_value(
+    dct: Mapping, key: Any, default: Union[T, None] = None
+) -> Union[Any, T]:
+    """
+    Safely retrieve a value from a dictionary by key,
+    returning a default value if the key is not found.
+
+    Arguments:
+        dct (Mapping): A dictionary-like mapping from which retrieve the value
+        key (Any): The key to search for in the dictionary.
+        default (Union[T, None], optional): The default value to return
+            if the key is not found. Defaults to None.
+
+    Returns:
+       Union[Any, T]: The value corresponding to the key, or the default value
+    """
+    if key in dct:
+        return dct[key]
+    return default
