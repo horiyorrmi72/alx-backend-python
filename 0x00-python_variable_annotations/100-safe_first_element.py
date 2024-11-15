537594c0bfe998ba592cc51b6c4d034852f1bcea
yusuf ridwan <horiyorrmi72@gmail.com>
2024-11-15 10:20:29 +0000

    refactor(docs): This commit will add more documentation to the code

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
-"""Duck-typed Module"""
+"""Duck-typed Module that safely retrieves the first element
+from a sequence, returning None if the sequence is empty"""
 from typing import Sequence, Any, Union, Optional
 
 
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """
+    Safely retrieve the first element of a sequence.
+    """
     if lst:
         return lst[0]
     else:
