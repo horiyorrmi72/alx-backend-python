7a4631f340e1f5ce2b75be8636ddc2cd274c557b
yusuf ridwan <horiyorrmi72@gmail.com>
2024-11-15 12:08:15 +0000

    feat(async basics): this commit will make use of random and asyncio module to execute an async function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+"""
+This module defines an asynchronous`wait_random` that waits for a
+random delay and returns the delay time.
+"""
 
 import asyncio
 import random
 
 
-async def wait_random(max_delay: float = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """ returns random float numbers betweewn 0 and max_delay
 
 max_delay: the maximum delay time to return
