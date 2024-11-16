7f8dd342c3d0ba2c61cfae5bd5707f379de75095
yusuf ridwan <horiyorrmi72@gmail.com>
2024-11-16 06:21:10 +0000

    feat: implement async_generator coroutine

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@ This module defines `async_generator` which yields random numbers
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None,  None]:
     """
     Asynchronously generates 10 random numbers between 0 and 10.
 
